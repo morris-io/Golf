80916ed86d394a6ee91da1c6b9c7304e489f2b47
Michael <mchaelmorris@gmail.com>
2026-02-04 21:04:48 -0500

    top color for app view

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="The ultimate fantasy golf drafting app" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <meta name="theme-color" content="#22c55e" />
+        <meta name="theme-color" content="#ABE2F6" />
       </Head>
       <Component {...pageProps} />
     </>
