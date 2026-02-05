f5f2f8d07265b3b885a9520f66c7af7cef27d0b8
Michael <mchaelmorris@gmail.com>
2026-02-04 19:24:45 -0500

    installable app

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in dev mode
+  disable: process.env.NODE_ENV === "development",
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // This helps prevent memory crashes during build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = withPWA(nextConfig);
